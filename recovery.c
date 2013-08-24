3ea9b8ad82feb20bb864a409846a0a7dcedc6f9a
jeboo <jeb00o@yahoo.com>
2013-08-24 12:18:29 -0400

    version 1.1

--- recovery.c
+++ recovery.c
@@ -418,7 +418,7 @@ copy_sideloaded_package(const char* original_path) {
 
 static char**
 prepend_title(char** headers) {
-    char* title[] = { EXPAND(RECOVERY_VERSION),
+    char* title[] = { "Jeboo Recovery v1.1",
                       "",
                       NULL };
 
